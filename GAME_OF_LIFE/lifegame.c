675f53779941602f2e40985d51a8adbfdda449ff
peter <jengku@gmail.com>
2016-04-15 20:17:59 +0800

    BUG BUG BUG

--- GAME_OF_LIFE/lifegame.c
+++ GAME_OF_LIFE/lifegame.c
@@ -3,23 +3,24 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <time.h>
-#define MAX 43
+#define HMAX 43
+#define WMAX 150
 
 const int dire[8][2]={{0,-1},{0,1},{1,-1},{1,0},{1,1},{-1,-1},{-1,0},{-1,1}};
 
-void prtout(bool map[MAX][MAX])
+void prtout(bool map[HMAX][WMAX])
 {
 	int i,j;
 	for(i=0;i<100;i++)printf("\n");
-	for(i=0;i<MAX;i++)
+	for(i=0;i<HMAX;i++)
 	{
-		for(j=0;j<MAX;j++)
+		for(j=0;j<WMAX;j++)
 			printf(map[i][j]?"#":".");
 		printf("\n");
 	}
 }
 
-int live(const int x, const int y, bool map[MAX][MAX])
+int live(const int x, const int y, bool map[HMAX][WMAX])
 {
 	int count=0;
 	int tx,ty;
@@ -28,12 +29,12 @@ int live(const int x, const int y, bool map[MAX][MAX])
 	{
 		tx=x+dire[i][0];
 		ty=y+dire[i][1];
-		if(tx>=0&&tx<MAX&&ty>=0&&ty<MAX&&map[ty][tx])count++;
+		if(tx>=0&&tx<WMAX&&ty>=0&&ty<HMAX&&map[ty][tx])count++;
 	}
 	return count;
 }
 
-void grow(const int x, const int y, bool map1[MAX][MAX], bool map2[MAX][MAX])
+void grow(const int x, const int y, bool map1[HMAX][WMAX], bool map2[HMAX][WMAX])
 {
 	int n=live(x,y,map1);
 	if(map1[y][x])
@@ -53,11 +54,12 @@ int main(void)
 	int n, i, j, k;
 	int tx,ty,t;
 	FILE *fptr;
-	char temp[MAX+2];
+	char temp[WMAX+2];
 	srand(time(NULL));
-	bool map[2][MAX][MAX];
-	for(i=0; i<MAX; i++)for(j=0;j<MAX;j++)for(k=0;k<2;k++)
+	bool map[2][HMAX][WMAX];
+	for(i=0; i<HMAX; i++)for(j=0;j<WMAX;j++)for(k=0;k<2;k++)
 		map[k][i][j]=false;
+	for(i=0;i<WMAX;i++)temp[i]=0;
 	scanf("%d%d",&n,&t);
 	if(n<=0)
 	{
@@ -65,10 +67,10 @@ int main(void)
 		if(fptr==NULL)return -1;
 		else
 		{
-			for(i=0;i<MAX;i++)
+			for(i=0;i<HMAX;i++)
 			{
 				fscanf(fptr,"%s",temp);
-				for(j=0;j<MAX;j++)
+				for(j=0;j<WMAX;j++)
 				{
 					if(temp[j]=='#')map[0][i][j]=true;
 					else map[0][i][j]=false;
@@ -79,14 +81,14 @@ int main(void)
 	}
 	else
 	{
-		if(n>MAX*MAX)n=MAX*MAX;
+		if(n>HMAX*WMAX)n=HMAX*WMAX;
 
 		for(i=0; i<n; i++)
 		{
 			while(1)
 			{
-				tx=rand()%MAX;
-				ty=rand()%MAX;
+				tx=rand()%WMAX;
+				ty=rand()%HMAX;
 				if(!map[0][ty][tx])
 				{
 					map[0][ty][tx]=true;
@@ -98,11 +100,11 @@ int main(void)
 	prtout(map[0]);
 	for(k=0; k<t; k++)
 	{
-		usleep(200000);
+		usleep(100000);
 		//getchar();
-		for(i=0;i<MAX;i++)for(j=0;j<MAX;j++)
+		for(i=0;i<HMAX;i++)for(j=0;j<WMAX;j++)
 		{
-			grow(i,j,map[k%2],map[(k+1)%2]);
+			grow(j,i,map[k%2],map[(k+1)%2]);
 		}
 		prtout(map[(k+1)%2]);
 	}
